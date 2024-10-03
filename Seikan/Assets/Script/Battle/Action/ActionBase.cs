e7ef970a80090a91c1d92c600592ade5bf7275cc
raichi0205 <raichi.q.0205@gmail.com>
2024-10-04 01:45:12 +0900

    各ステータスの配列をインスペクター上でEnumに合致した名前で表示するように追記 補正倍率の初期値を1に訂正

--- Seikan/Assets/Script/Battle/Action/ActionBase.cs
+++ Seikan/Assets/Script/Battle/Action/ActionBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Star.Character;
 using Cysharp.Threading.Tasks;
+using Star.Editor;
 
 namespace Star.Battle
 {
@@ -26,13 +27,16 @@ namespace Star.Battle
         public class Correction
         {
             public int Value = 0;           // 加減値
-            public float Rate = 0;          // 補正倍率
+            public float Rate = 1;          // 補正倍率
         }
 
         public int DefaultTargetNum = int.MinValue;
         [SerializeField] protected Action_Type actionType = Action_Type.None;
         public Action_Type ActionType { get { return actionType; } }
-        [SerializeField] protected Correction[] corrections = new Correction[(int)Status.NUM];     // 各ステータスの補正データ
+
+        [SerializeField, NamedArray(typeof(Status))]
+        protected Correction[] corrections = new Correction[(int)Status.NUM];     // 各ステータスの補正データ
+        
         public CharacterBase Chara;        // 行動主のキャラデータ
         protected CharacterBase target = null;
 
