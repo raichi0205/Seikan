5d94fe22e292f1fe2462d85e178e3881bd66970b
raichi0205 <raichi.q.0205@gmail.com>
2024-10-04 01:45:40 +0900

    防御行動の優先度を100に引き上げて現状最優先に

--- Seikan/Assets/Script/Battle/Action/ActionGuard.cs
+++ Seikan/Assets/Script/Battle/Action/ActionGuard.cs
@@ -8,10 +8,14 @@ namespace Star.Battle
     [CreateAssetMenu(fileName = "Guard", menuName = "Battle/Action/ActionGuard", order = 0)]
     public class ActionGuard : ActionBase
     {
+        int delayTime = 1000;
+
         public override async UniTask Action(CharacterBase _target)
         {
             BattleSystem.Instance.SystemMsg = "身を守っている";
             base.Action(_target);
+
+            await UniTask.Delay(delayTime);
         }
     }
 }
