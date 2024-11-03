4578a5f6339d482a34edab999a3c00d87d481f93
raichi0205 <raichi.q.0205@gmail.com>
2024-11-04 04:13:56 +0900

    スキル用エフェクトの追加と、既存のファイル名の調整

--- Seikan/Assets/Script/Lua/LuaScript/Skill/TestSkill.lua
+++ Seikan/Assets/Script/Lua/LuaScript/Skill/TestSkill.lua
@@ -5,7 +5,7 @@ TestSkill.new = function ()
         startCoroutine(function ()
             Skill.SystemMsg("skill used")
             local skill = CS.Star.Battle.ActionSkill.CurrentSkill
-            coroutine.yield(skill:PlayEffect("Attack_01"))
+            coroutine.yield(skill:PlayEffect("Skill_01"))
             print("エフェクト終了")
             skill.IsEnd = true
         end)
