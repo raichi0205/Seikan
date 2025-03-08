2c3cfedfda6eda74221fefb1806a9a77b261fcad
raichi0205 <raichi.q.0205@gmail.com>
2025-03-08 22:04:36 +0900

    毒状態のエフェクト表示

--- Seikan/Assets/Script/Battle/State/PoisonState.cs
+++ Seikan/Assets/Script/Battle/State/PoisonState.cs
@@ -9,6 +9,7 @@ namespace Star.Battle
     {
         PoisonData poisonData;
         public PoisonData PoisonData { get { return poisonData; } set { stateData = poisonData = value; } }
+        const string effectName = "Poison_01";
 
         /// <summary>
         /// 状態の実行
@@ -19,6 +20,20 @@ namespace Star.Battle
         {
             int hp = _exeChara.GetStatus(Status.HP);            // 現在HP取得
             int subHp = (int)(hp * poisonData.DecreaseRate);    // 減少するHPの量を決める
+
+            // エフェクト再生
+            if(_exeChara.GetType() == typeof(Actor))
+            {
+                var effect = BattleSystem.Instance.BattleUIController.AllEffect;
+                Effect.SpriteEffectManager.Instance.SetEffect(effectName, effect);
+                effect.Play();
+                await effect.EndDelay();
+            }
+            else
+            {
+                await EnemyManager.Instance.PlayEffect(((Enemy)_exeChara).Num, effectName).ToCoroutine();
+            }
+
             _exeChara.SubCurrentStatus(Status.HP, subHp);       // 減少処理
             await _exeChara.UpdateGage(Status.HP);
             base.Execute(_exeChara);
